7299ee5338f7450e98a03420b04c42823d4fc7f7
reuben.baek <reuben.baek@daumkakao.com>
2016-11-27 15:58:29 +0900

    update test.

--- test/intellij-ts-mocha.test.js
+++ test/intellij-ts-mocha.test.js
@@ -4,34 +4,34 @@ describe('intellij-ts-mocha', function() {
     describe('#replaceCommand', function() {
         it('should remove intellij-ts-mocha and replace the last ts file extention as js.', function() {
             var debugCmd = [ '/usr/local/bin/node',
-                '/Users/reuben/IdeaProjects/typescript-learning/typescript-mocha/idea-node.js',
+                '/Users/reuben/IdeaProjects/intellij-ts-mocha/bin/intellij-ts-mocha',
                 '--debug-brk=63392',
-                '/Users/reuben/IdeaProjects/typescript-learning/typescript-mocha/node_modules/mocha/bin/_mocha',
+                '/Users/reuben/IdeaProjects/intellij-ts-mocha/node_modules/mocha/bin/_mocha',
                 '--timeout',
                 '259200000',
                 '--ui',
                 'bdd',
                 '--reporter',
                 '/Users/reuben/Library/Application Support/IntelliJIdea15/NodeJS/js/mocha-intellij/lib/mochaIntellijReporter.js',
-                '/Users/reuben/IdeaProjects/typescript-learning/typescript-mocha/test/calculator.test.ts',
+                '/Users/reuben/IdeaProjects/typescript-learning/test/calculator.test.ts',
                 '--grep',
                 'Calculator ' ];
             var expectedCmd = [ '/usr/local/bin/node',
                 '--debug-brk=63392',
-                '/Users/reuben/IdeaProjects/typescript-learning/typescript-mocha/node_modules/mocha/bin/_mocha',
+                '/Users/reuben/IdeaProjects/intellij-ts-mocha/node_modules/mocha/bin/_mocha',
                 '--timeout',
                 '259200000',
                 '--ui',
                 'bdd',
                 '--reporter',
                 '\"/Users/reuben/Library/Application Support/IntelliJIdea15/NodeJS/js/mocha-intellij/lib/mochaIntellijReporter.js\"',
-                '/Users/reuben/IdeaProjects/typescript-learning/typescript-mocha/test/calculator.test.js',
+                '/Users/reuben/IdeaProjects/typescript-learning/test/calculator.test.js',
                 '--grep',
                 '\"Calculator \"' ];
             var actualCmd = replaceCommand(debugCmd);
 
             if (expectedCmd.join(" ") !== actualCmd.join(" ")) {
-                throw new Error(`Error!! expected = ${expectedCmd} but actual = ${actualCmd}`);
+                throw new Error(`Error!! \nexpected = ${expectedCmd}\nactual = ${actualCmd}`);
             }
         })
     });
